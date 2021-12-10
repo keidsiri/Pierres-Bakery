1a72b46be9f7878cba1e8095df7194396a75a0ab
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-10 14:47:51 -0800

    pass pastry test 6th test, rreturn correct price with promo discount

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -53,6 +53,16 @@ namespace PierresBakery.Tests
         Assert.AreEqual(PastryPrice, newOrder);
       }
 
+    [TestMethod]
+      public void Pastry_ReturnCostOfSixPastry_Int()
+      {
+        int PastryAmount = 6;
+        int PastryPrice  = 10;
+        Pastry newPastry = new Pastry(PastryAmount);
+        int newOrder = newPastry.PastryCost();
+        Assert.AreEqual(PastryPrice, newOrder);
+      }
+
 
 
   }
