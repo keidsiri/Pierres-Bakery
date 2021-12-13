2230da4e4ea3a59067611c6eb4aa29511fc732b9
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-12 17:51:08 -0800

    update ui

--- PierresBakery/Models/Program.cs
+++ PierresBakery/Models/Program.cs
@@ -16,20 +16,59 @@ class Program
     Console.WriteLine("Second :  We have Pastry for $2 each \n     \n ** Promotion now! 3 Pastries for $5 ** ");
     Console.WriteLine("--------------------------------------------------\n");
     Console.WriteLine("How many 'Bread' would you like to get?");
-    int breadOrder= int.Parse(Console.ReadLine());
-    Bread newBread = new Bread(breadOrder);
-    int breadCost = newBread.BreadCost();
+    string FirstOrder = Console.ReadLine();
+    
 
-    Console.WriteLine("How many 'Pastry' would you like to get?");
-    int pastryOrder = int.Parse(Console.ReadLine());
-    Pastry newPastry = new Pastry(pastryOrder);
-    int pastryCost = newPastry.PastryCost();
+    if ( FirstOrder == "1"|| FirstOrder == "2" || FirstOrder == "3" || FirstOrder == "4" || FirstOrder == "5" ||
+        FirstOrder == "6" || FirstOrder == "7" || FirstOrder == "8" || FirstOrder == "9" || FirstOrder == "0" || FirstOrder == "11" || FirstOrder == "12" || FirstOrder == "13" || FirstOrder == "14" || FirstOrder == "15" || FirstOrder == "16" || FirstOrder == "17" || FirstOrder == "18" || FirstOrder == "19" || FirstOrder == "20" || FirstOrder == "21" || FirstOrder == "22" || FirstOrder == "23" || FirstOrder == "24" || FirstOrder == "25" || FirstOrder == "26" || FirstOrder == "27" || FirstOrder == "28" || FirstOrder == "29" || FirstOrder == "30")
+    {
+      int breadOrder = int.Parse(FirstOrder);
+      Bread newBread = new Bread(breadOrder);
+      int BreadCost = newBread.BreadCost();
 
-    int total = breadCost + pastryCost;
-    Console.WriteLine($"Your order is {breadOrder} Bread(s) {pastryOrder}, Pastries and the total is ${total}");
-    Console.WriteLine("");
-    Console.WriteLine($"Thank you for your support! We hope to see you again soon!");
+      Console.WriteLine("How many 'Pastry' would you like to get?");
+      string SecondOrder = Console.ReadLine();
+      
+      if ( SecondOrder  == "1" || SecondOrder  == "2" || SecondOrder == "3" ||SecondOrder  == "4" || SecondOrder == "5" ||
+        SecondOrder == "6" || FirstOrder == "7" || FirstOrder == "8" || FirstOrder == "9" || FirstOrder == "0" || FirstOrder == "11" || FirstOrder == "12" || FirstOrder == "13" || FirstOrder == "14" || FirstOrder == "15" || FirstOrder == "16" || FirstOrder == "17" || FirstOrder == "18" || FirstOrder == "19" || FirstOrder == "20" || FirstOrder == "21" || FirstOrder == "22" || FirstOrder == "23" || FirstOrder == "24" || FirstOrder == "25" || FirstOrder == "26" || FirstOrder == "27" || FirstOrder == "28" || FirstOrder == "29" || FirstOrder == "30")
+      {
+        int pastryOrder = int.Parse(SecondOrder);
+        Pastry newPastry = new Pastry(pastryOrder);
+        int PastryCost = newPastry.PastryCost();
+        int totalCost = BreadCost + PastryCost;
+        Console.WriteLine($"Your order total is {totalCost} dollars");
+      }
+      else
+      {
+        Console.WriteLine("Would you like to make an order?");
+        string answer = Console.ReadLine();
+        if ( answer.ToLower() == "yes" || answer.ToLower() == "yeah" || answer.ToLower() == "yea" || answer.ToLower() == "sure")
+        {
+          Main();
+        }
+        else 
+        {
+          Console.WriteLine("We hope to see you soon!");
+        }
+      }
+    }
+    else 
+    {
+      Console.WriteLine("Please only enter the number for your order!!");
+      Console.WriteLine("Would you like to make an order?");
+      string answer = Console.ReadLine();
+      if ( answer.ToLower() == "yes" || answer.ToLower() == "yeah" || answer.ToLower() == "yea" || answer.ToLower() == "sure")
+      {
+        Main();
+      }
+      else 
+      {
+        Console.WriteLine("We hope to see you soon!");
+      }
+    }
+
   }
 }
+
 
   
