e691c3595eef2b91409176a1b44fb11d2952a158
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-12 17:23:45 -0800

    Change the UI in Program.cs

--- PierresBakery/Models/Program.cs
+++ PierresBakery/Models/Program.cs
@@ -7,60 +7,28 @@ class Program
   public static void Main()
   {
     Console.WriteLine("_____________________________________________________________________________\n");
-    Console.WriteLine("Welcome to Pieerie's Bakery shop. We would love you to try our handmade bakery");
+    Console.WriteLine("Welcome to Pierre's Bakery shop. We would love you to try our handmade bakery");
     Console.WriteLine("");
     Console.WriteLine("______________Fresh______________Bakery______________Everyday________________\n");
     Console.WriteLine(""); 
-    Console.WriteLine("First  :  We have Bread a single loaf is $5 \n ** Promotion now! Buy 2 get 1 Free!! **");
+    Console.WriteLine("First  :  We have Bread a single loaf is $5 \n  \n ** Promotion now! Buy 2 get 1 Free!! **");
     Console.WriteLine("--------------------------------------------------\n");
-    Console.WriteLine("Second :  We have Pastry for $2 each \n ** Promotion now! 3 Pastries for $5 ** ");
+    Console.WriteLine("Second :  We have Pastry for $2 each \n     \n ** Promotion now! 3 Pastries for $5 ** ");
     Console.WriteLine("--------------------------------------------------\n");
-    Console.WriteLine("How many Bread would you like to get?");
-    string FirstOrder = Console.ReadLine();
-    
+    Console.WriteLine("How many 'Bread' would you like to get?");
+    int breadOrder= int.Parse(Console.ReadLine());
+    Bread newBread = new Bread(breadOrder);
+    int breadCost = newBread.BreadCost();
 
-    if ( FirstOrder == "1"|| FirstOrder == "2" || FirstOrder == "3" || FirstOrder == "4" || FirstOrder == "5" ||
-        FirstOrder == "6" || FirstOrder == "7" || FirstOrder == "8" || FirstOrder == "9" || FirstOrder == "0")
-    {
-      int breadOrder = int.Parse(FirstOrder);
-      Bread newBread = new Bread(breadOrder);
-      int BreadCost = newBread.BreadCost();
+    Console.WriteLine("How many 'Pastry' would you like to get?");
+    int pastryOrder = int.Parse(Console.ReadLine());
+    Pastry newPastry = new Pastry(pastryOrder);
+    int pastryCost = newPastry.PastryCost();
 
-      Console.WriteLine("How many Pastry would you like to get?");
-      string SecondOrder = Console.ReadLine();
-      
-      if ( SecondOrder  == "1" || SecondOrder  == "2" || SecondOrder == "3" ||SecondOrder  == "4" || SecondOrder == "5" ||
-        SecondOrder == "6" || SecondOrder == "7" || SecondOrder  == "8" || SecondOrder  == "9" || SecondOrder == "0")
-      {
-        int pastryOrder = int.Parse(SecondOrder);
-        Pastry newPastry = new Pastry(pastryOrder);
-        int PastryCost = newPastry.PastryCost();
-        int totalCost = BreadCost + PastryCost;
-        Console.WriteLine($"Your order total is {totalCost} dollars");
-      }
-      else
-      {
-        Console.WriteLine("Please only enter the number for your order.Goog bye!!!");
-      }
-    }
-    else 
-    {
-      Console.WriteLine("Please only enter the number for your order!!");
-      Console.WriteLine("Would you like to make an order?");
-      string answer = Console.ReadLine();
-      if ( answer.ToLower() == "yes" || answer.ToLower() == "yeah" || answer.ToLower() == "yea" || answer.ToLower() == "sure")
-      {
-        Main();
-      }
-      else 
-      {
-        Console.WriteLine("We hope to see you soon!");
-      }
-    }
-
+    int total = breadCost + pastryCost;
+    Console.WriteLine($"Your order is {breadOrder} Bread(s) {pastryOrder}, Pastries and the total is ${total}");
+    Console.WriteLine($"Thank you for your support! We hope to see you again soon!");
   }
 }
 
-    
-  
   
